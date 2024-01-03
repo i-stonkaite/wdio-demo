368fdac53adce9f7be2f819ed23f8f37810724e9
i-stonkaite <i.stonkaite@protonmail.com>
2024-01-03 15:43:38 +0200

    redo to single quotes

--- test/specs/about.js
+++ test/specs/about.js
@@ -1,10 +1,15 @@
-describe("About", () => {
-  it("Open About Page & assert URL", async () => {
-    await browser.url("https://practice.sdetunicorns.com/");
-    await $("#menu-item-491").click();
+describe('About', () => {
+  it('Open About Page & assert URL', async () => {
+    await browser.url('https://practice.sdetunicorns.com/');
+    await $('#menu-item-491').click();
 
-    // const currentUrl = await browser.getUrl();
+    await expect(browser).toHaveUrl('https://practice.sdetunicorns.com/about/');
+  });
 
-    await expect(browser).toHaveUrl("https://practice.sdetunicorns.com/about/");
+  it('Click Get Started button & assert url contains get-started text', async () => {
+    await browser.url('https://practice.sdetunicorns.com/');
+
+    await $('#get-started').click();
+    await expect(browser).toHaveUrl('get-started/');
   });
 });
--- test/specs/home.js
+++ test/specs/home.js
@@ -1,12 +1,12 @@
 // Practice E-Commerce Site – SDET Unicorns – Helping you succeed in Software Quality.
 // https://practice.sdetunicorns.com/
 
-describe("Home", () => {
-  it("Open URL & assert title", async () => {
-    await browser.url("https://practice.sdetunicorns.com/");
+describe('Home', () => {
+  it('Open URL & assert title', async () => {
+    await browser.url('https://practice.sdetunicorns.com/');
 
     await expect(browser).toHaveTitle(
-      "Practice E-Commerce Site – SDET Unicorns – Helping you succeed in Software Quality."
+      'Practice E-Commerce Site – SDET Unicorns – Helping you succeed in Software Quality.'
     );
   });
 });

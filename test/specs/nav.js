78bbecb162b02f319e8565c1f8a0a09bd688cc64
i-stonkaite <i.stonkaite@protonmail.com>
2024-01-04 19:39:29 +0200

    add wait until example

--- test/specs/nav.js
+++ test/specs/nav.js
@@ -13,6 +13,18 @@ describe('Navigation Menu', () => {
 
     const actualLinks = [];
 
+    // wait for the Home text to be displayed on the nav menu
+    await browser.waitUntil(
+      async function () {
+        const homeText = await $('#zak-primary-menu li').getText();
+        return homeText === 'Home';
+      },
+      {
+        timeout: 5000,
+        timeoutMsg: 'Could not verify the Home text from #zak-primary-menu li',
+      }
+    );
+
     const navLinks = await $('#zak-primary-menu').$$('li[id*=menu]');
 
     for (let link of navLinks) {
